d7a2b21282c28f2defe7958f7a0480d883472d26
R. Kyle Murphy <orclev@gmail.com>
2021-06-08 20:29:49 -0400

    feat: add build script for *nix systems

---
+++ blitgin-make.sh
@@ -0,0 +1,8 @@
+rm harness/public/blitgin_bg.wasm
+rm harness/public/blitgin.js
+cargo clean
+cargo build --release
+# wasm-gc target/wasm32-unknown-unknown/release/blitgin.wasm
+wasm-pack build --target no-modules --out-dir target/bindgen/output
+cp target/bindgen/output/blitgin_bg.wasm harness/public/blitgin_bg.wasm
+cp target/bindgen/output/blitgin.js harness/public/blitgin.js
